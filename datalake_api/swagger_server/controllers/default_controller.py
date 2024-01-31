a2c3edf0c18b744fa121947c0ce7c285199df4a6
Luca Babetto <l.babetto@cineca.it>
2024-01-31 11:08:06 +0100

    test

--- datalake_api/swagger_server/controllers/default_controller.py
+++ datalake_api/swagger_server/controllers/default_controller.py
@@ -497,7 +497,7 @@ def upload_post(file, json_data, **kwargs):
 
         # NOTE: upload_file was changes to upload_fileobject
         # if file is necessary to TUI it might be useful to recover also upload_file
-        print(file.stream.read())
+        print(file.filename)
         s3.upload_fileobj(
             Fileobj=file.stream,
             Bucket=env_config.get("S3_BUCKET"),
