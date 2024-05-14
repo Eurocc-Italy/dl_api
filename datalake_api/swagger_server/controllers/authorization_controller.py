aa5f5cedd17e13795c3c1c0434b99eda09c0b4d8
lbabetto <l.babetto@cineca.it>
2024-05-14 09:44:15 +0200

    added token generator

--- datalake_api/swagger_server/controllers/authorization_controller.py
+++ datalake_api/swagger_server/controllers/authorization_controller.py
@@ -1,4 +1,5 @@
 from typing import List
+import argparse
 
 """
 controller generated to handled auth operation described at:
@@ -23,6 +24,7 @@ JWT_SECRET = env_config.get("JWT_SECRET")
 JWT_LIFETIME_SECONDS = int(env_config.get("JWT_LIFETIME_SECONDS"))
 JWT_ALGORITHM = env_config.get("JWT_ALGORITHM")
 
+
 def decode_token(token):
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
@@ -31,15 +33,43 @@ def decode_token(token):
         print(f"JWT Decode Error: {e}")
         raise Unauthorized from e
 
-def generate_token(user_id):
+
+def generate_token(user_id, duration=JWT_LIFETIME_SECONDS):
     timestamp = _current_timestamp()
     payload = {
         "iss": JWT_ISSUER,
         "iat": int(timestamp),
-        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+        "exp": int(timestamp + duration),
         "sub": str(user_id),
     }
     return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+
+def generate_token_cli():
+    parser = argparse.ArgumentParser(description="Authentication token generation for Cineca's Data Lake as a Service.")
+
+    parser.add_argument(
+        "--user",
+        help="Name of the user associated with the token",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--duration",
+        help="duration of the authentication token, in seconds",
+        default=JWT_LIFETIME_SECONDS,
+        required=False,
+    )
+
+    args = parser.parse_args()
+    token = generate_token(user_id=args.user, duration=int(args.duration))
+
+    print("\nGenerating authentication token. Please save the token and provide it to the intended user.")
+    print("Default location for automatic token retrieval: ~/.config/dlaas/api-token.txt\n")
+
+    print(f"  User: {args.user}")
+    print(f"  Duration: {args.duration} seconds")
+    print(f"  Token: {token}\n")
 
 
 def get_secret(user, token_info) -> str:
