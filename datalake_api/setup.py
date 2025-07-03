c61533bc678caf22f40e5ed4174b22eaf6fb06eb
lbabetto <l.babetto@cineca.it>
2025-07-03 15:15:46 +0200

    Version bump

--- datalake_api/setup.py
+++ datalake_api/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "swagger_server"
-VERSION = "1.1"
+VERSION = "1.2"
 # To install the library, run the following
 #
 # python setup.py install
